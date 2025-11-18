d2136d7fe590cad6999d3457e39e8f7b430a9cc5
Juan Cruz Garetto <jggaretto@gmail.com>
2025-11-18 00:31:53 -0300

    modificaciones en main

--- src/Main.java
+++ src/Main.java
@@ -1,38 +1,38 @@
 public class Main {
     public static void main(String[] args) {
-        Profesor prof1 = new Profesor("Fernando", "Rosales", 45, "10223344", "Matemática", 20);
-        Profesor prof2 = new Profesor("Walter", "Magallanes", 50, "11222333", "Programación", 25);
+        Profesor profesor1 = new Profesor("Fernando", "Rosales", 45, "10223344", "Matemática", 20);
+        Profesor profesor2 = new Profesor("Walter", "Magallanes", 50, "11222333", "Programación", 25);
 
         Materia[] materias1 = {
-            new Materia("Matemática I", 7.5, prof1),
-            new Materia("Programación", 9.0, prof2)
+            new Materia("Matemática I", 7.5, profesor1),
+            new Materia("Programación", 9.0, profesor2)
         };
         Materia[] materias2 = {
-            new Materia("Matemática II", 8.5, prof1),
-            new Materia("Programación Avanzada", 9.5, prof2)
+            new Materia("Matemática II", 8.5, profesor1),
+            new Materia("Programación Avanzada", 9.5, profesor2)
         };
 
-        Estudiante e1 = new Estudiante("Guillermina", "Hernandez", 20, "45111222", "Ingeniería", materias1);
-        Estudiante e2 = new Estudiante("Juan", "Garetto", 21, "44954668", "Sistemas", materias2);
+        Estudiante estudiante1 = new Estudiante("Guillermina", "Hernandez", 20, "45111222", "Ingeniería", materias1);
+        Estudiante estudiante2 = new Estudiante("Juan", "Garetto", 21, "44954668", "Sistemas", materias2);
 
-        Personal p1 = new Personal("Antonella", "Martínez", 38, "33111444", "Administración", "Secretaria", "2010-03-12");
+        Personal personal1 = new Personal("Antonella", "Martínez", 38, "33111444", "Administración", "Secretaria", "2010-03-12");
 
-        Universidad u = new Universidad();
-        u.agregarMiembro(e1);
-        u.agregarMiembro(e2);
-        u.agregarMiembro(prof1);
-        u.agregarMiembro(prof2);
-        u.agregarMiembro(p1);
+        Universidad universidad = new Universidad();
+        universidad.agregarMiembro(estudiante1);
+        universidad.agregarMiembro(estudiante2);
+        universidad.agregarMiembro(profesor1);
+        universidad.agregarMiembro(profesor2);
+        universidad.agregarMiembro(personal1);
 
-        System.out.println("=== Listado de Miembros ===");
-        u.listarMiembros();
+        System.out.println("Listado de Miembros: ");
+        universidad.listarMiembros();
 
-        System.out.println("\n=== Ordenamiento por Apellido ===");
-        Estudiante[] lista = {e1, e2};
+        System.out.println("\nOrdenamiento por Apellido:");
+        Estudiante[] lista = {estudiante1, estudiante2};
         Universidad.ordenarEstudiantesPorApellido(lista);
-        for (Estudiante e : lista) System.out.println(e);
+        for (Estudiante estudiante : lista) System.out.println(estudiante);
 
-        System.out.println("\n=== Búsqueda Binaria (Apellido = 'Garetto') ===");
+        System.out.println("\nBúsqueda Binaria (Apellido = 'Garetto') ");
         int pos = Universidad.busquedaBinariaEstudiantes(lista, "Garetto");
         System.out.println(pos >= 0 ? "Encontrado: " + lista[pos] : "No encontrado");
     }
